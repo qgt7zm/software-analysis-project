53bad889862e39f3997038dae8d4dfe91e6c794f
qgt7zm <qgt7zm@virginia.edu>
2026-04-22 23:00:14 -0400

    Add project config files

---
+++ gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("net.ltgt.errorprone") version "5.1.0"
+}
+
+tasks.withType<JavaCompile> {
+    options.errorprone  {
+        disableAllChecks = true // Disable other checks
+        option("NullAway:OnlyNullMarked", "true") // Only check null-marked code
+        // option("NullAway:AnnotatedPackages", "package1,package2") // Only check given packages
+        error("NullAway") // Use warning level
+        option("NullAway:JSpecifyMode", "true") // JSpecify support
+        // option("NullAway:JSpecifyMode", "true") // Assert statements are ignored by default
+    }
+}
+
+dependencies {
+    implementation("org.jspecify:jspecify:1.0.0")
+    errorprone("com.google.errorprone:error_prone_core:2.48.0")
+    errorprone("com.uber.nullaway:nullaway:0.31.1")
+}
